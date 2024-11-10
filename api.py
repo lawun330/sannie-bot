fe250aa7e49cc2ec64b6cdd45121d32c0b28fae5
La Wun Nannda <lawun330@gmail.com>
2024-11-10 17:02:44 +0700

    update code, improve readability

--- api.py
+++ api.py
@@ -4,11 +4,25 @@ It uses redis to cache the data so that the data is not scraped again.
 
 There is a series of scraping process:
 - scrape topic links from the main page (already done)
-- scrape page links from a topic link
-- scrape content links from a page link
-- scrape content from a content link
+- scrape links of pages from a topic link
+- scrape links of contents from a page link
+- scrape content/article from a content link
 
 Topic links are already associated with the dropdown menu in the index page.
+The user can select a topic from the dropdown menu to view the pages of the topic.
+The user can select a page to view all available contents of the page.
+The user can select a content url to copy and return to the index page.
+The user can insert a url in the index page to read the content/article in the article page.
+
+The API is used by the following files:
+- index.js: to get topic links and display them in the dropdown menu
+- loading.js: to test data completion by fetching data from the cache to continue the redirecting process
+- pages.js: to get page links and display them in the container
+- contents.js: to get content links and display them in the container
+- article.js: to get content/article and display it
+
+In each endpoint, the data is serialized to JSON strings to be stored in Redis because Redis can only store strings.
+In each endpoint, the data is deserialized back to Python objects before being returned to the client.
 '''
 
 # import libraries
@@ -21,7 +35,7 @@ import json
 # import custom functions from webscraping modules
 from webscraper.modules import fetch_pages  # B_pages_scraper
 from webscraper.modules import fetch_content_links  # C_content_links_scraper
-from webscraper.modules import get_content  # D_content_scraper
+from webscraper.modules import get_article  # D_content_or_article_scraper
 
 # create FastAPI app
 app = FastAPI()
@@ -33,6 +47,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
+    expose_headers=["*"]  # Exposes all headers
 )
 
 # create Redis client
@@ -94,6 +109,7 @@ async def set_content(request: Request):
     except Exception as e:
         return {"error": f"Unexpected error: {str(e)}"}
 
+
 # DEBUGGING
 # read the chosen topic, page, and content from cache with fastAPI
 @app.get("/get_chosen_topic")
@@ -114,14 +130,14 @@ def read_content():
 
 # GETTING CHOSEN TOPIC, PAGE, AND CONTENT
 
-# WORKING WITH CHOSEN TOPIC
-# /pages contains the page links of the chosen topic
-    # cache the page links in Redis
-    # retrieve the page links from cache
+# A. WORKING WITH CHOSEN TOPIC
+# /pages contains links of pages of the chosen topic
+    # cache the links of pages in Redis
+    # retrieve the links of pages from cache
 @app.get("/pages")
 def read_pages():
-    topic_link = redis_client.get(redis_javascript_cache_keys[0]) # get topic link from cache
-    try:    # try scraping the page links
+    topic_link = redis_client.get(redis_javascript_cache_keys[0]) # get chosen topic link from cache
+    try:    # try scraping the links of pages
         data = fetch_pages(topic_link)
     except:   # if connection error, retry until the connection comes back
         error_found = True
@@ -129,48 +145,47 @@ def read_pages():
             print("  - Connection Error. Retrying in 5 seconds...")
             time.sleep(5) # wait 5 seconds
             data = fetch_pages(topic_link) # try scraping again
-    redis_client.set(redis_scraped_cache_keys[0], json.dumps(data))  # Serialize the data to a JSON string
-    cache = redis_client.get(redis_scraped_cache_keys[0]) # get pages from cache
+    redis_client.set(redis_scraped_cache_keys[0], json.dumps(data))  # Serialize data to JSON string to cache links of pages
+    cache = redis_client.get(redis_scraped_cache_keys[0]) # get links of pages from cache
     return json.loads(cache)  # Deserialize the JSON string back to a Python object
 
-# WORKING WITH CHOSEN PAGE
-# /contents contains the content links of the chosen page
-    # cache the content links in Redis
-    # retrieve the content links from cache
+# B. WORKING WITH CHOSEN PAGE
+# /contents contains links of contents of the chosen page
+    # cache the links of contents in Redis
+    # retrieve the links of contents from cache
 @app.get("/contents")
-def read_content_links():
-    page_link = redis_client.get(redis_javascript_cache_keys[1]) # get page link from cache
-    try:    # try scraping the content links
+def read_contents():
+    page_link = redis_client.get(redis_javascript_cache_keys[1])  # get chosen page link from cache
+    try:    # try scraping the links of contents
         data = fetch_content_links(page_link)
     except:   # if connection error, retry until the connection comes back
         error_found = True
         while(error_found):
             print("  - Connection Error. Retrying in 5 seconds...")
-            time.sleep(5) # wait 5 seconds
-            data = fetch_content_links(page_link) # try scraping again
-    redis_client.set(redis_scraped_cache_keys[1], data) # set content links in cache
-    cache = redis_client.get(redis_scraped_cache_keys[1]) # get content links from cache
-    return cache
+            time.sleep(5)  # wait 5 seconds
+            data = fetch_content_links(page_link)  # try scraping again
+    redis_client.set(redis_scraped_cache_keys[1], json.dumps(data))  # Serialize to JSON string to cache links of contents
+    cache = redis_client.get(redis_scraped_cache_keys[1])  # get links of contents from cache
+    return json.loads(cache)  # Deserialize back to Python object
 
-
-# WORKING WITH CHOSEN CONTENT
-# /article contains the content of the chosen content link
-    # cache the content in Redis
-    # retrieve the content from cache
+# C. WORKING WITH CHOSEN CONTENT/ARTICLE
+# /article contains the content/article of the chosen content link
+    # cache the content/article in Redis
+    # retrieve the content/article from cache
 @app.get("/article")
-def read_content():
-    content_link = redis_client.get(redis_javascript_cache_keys[2]) # get content link from cache
-    try:    # try scraping the content
-        data = get_content(content_link)
+def read_article():
+    content_link = redis_client.get(redis_javascript_cache_keys[2]) # get chosen content link from cache 
+    try:    # try scraping the content/article
+        data = get_article(content_link)
     except:   # if connection error, retry until the connection comes back
         error_found = True
         while(error_found):
             print("  - Connection Error. Retrying in 5 seconds...")
             time.sleep(5) # wait 5 seconds
-            data = get_content(content_link) # try scraping again
-    redis_client.set(redis_scraped_cache_keys[2], data) # set content in cache
-    cache = redis_client.get(redis_scraped_cache_keys[2]) # get content from cache
-    return cache
+            data = get_article(content_link) # try scraping again
+    redis_client.set(redis_scraped_cache_keys[2], json.dumps(data)) # Serialize to JSON string to cache content/article
+    cache = redis_client.get(redis_scraped_cache_keys[2]) # get content/article from cache
+    return json.loads(cache) # Deserialize back to Python object
 
 
 # run the app
