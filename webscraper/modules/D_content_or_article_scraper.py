c867cff9e39a0a09dc384201d3691167b3d7f81c
La Wun Nannda <lawun330@gmail.com>
2024-11-10 14:56:39 +0700

    renamed file and function to match with api.py

--- webscraper/modules/D_content_or_article_scraper.py
+++ webscraper/modules/D_content_or_article_scraper.py
@@ -1,4 +1,4 @@
-'''This script is used to get the content from a particular content link.'''
+'''This script is used to get the content/article from a particular content/article link.'''
 
 import sys
 import os
@@ -12,17 +12,17 @@ from main import webScraper, contentScraper
 # test
 # execute only if the file is run as the main program
 if __name__ == "__main__":
-    # get a content link
+    # get a content/article link
     content_url = 'https://www.bbc.com/burmese/articles/clldd5j76pno' # example
 
-    # get the content from the content link
+    # get the content/article from the content/article link
     content_soup = webScraper(content_url)
     parsed_content = contentScraper(content_url, content_soup)
     print(parsed_content)
 
 
 # function implementation
-def get_content(content_url):
+def get_article(content_url):
     content_soup = webScraper(content_url)
     parsed_content = contentScraper(content_url, content_soup)
     return parsed_content
--- webscraper/modules/__init__.py
+++ webscraper/modules/__init__.py
@@ -1,4 +1,4 @@
 from .A_topics_scraper import fetch_topics
 from .B_pages_scraper import fetch_pages
 from .C_content_links_scraper import fetch_content_links
-from .D_content_scraper import get_content
+from .D_content_or_article_scraper import get_article
